24f25bd041e924d908324a14349314ca97332cc9
Kirill112002 <kirill_antonov1111@mail.ru>
2018-06-14 10:12:12 +0300

    Initial commit

---
+++ src/5.kt
@@ -0,0 +1,2 @@
+open class item(var Weapon: String, var Armor: String, var Potion: String, var MagicItem: String ){
+}
---
+++ src/main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val input1 = readLine()
+    val input2 = readLine()
+
+    if ((input1 != null) && (input2 != null)) {
+        print("Фамилия: ${input1},Имя: ${input2}")
+    }
+}
+
